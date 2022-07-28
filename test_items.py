c5d1a0a2a7b29e322df4ef60dd7dc819c56f7027
Phil <etozhephil@yandex.ru>
2022-07-28 20:52:34 +0300

    уменьшено время

--- test_items.py
+++ test_items.py
@@ -3,5 +3,5 @@ from selenium.webdriver.common.by import By
 def test_button_add_to_basket_exist_on_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(30)
+    time.sleep(5)
     assert browser.find_element(By.CSS_SELECTOR,'.btn.btn-lg.btn-primary.btn-add-to-basket')
