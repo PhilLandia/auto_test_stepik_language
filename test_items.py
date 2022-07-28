b68d69bb7030cb85d3c4b7a92afd703cd764f4f4
Phil <etozhephil@yandex.ru>
2022-07-28 20:34:00 +0300

    Создание автотеста на проверку языков на сайте

---
+++ test_items.py
@@ -0,0 +1,7 @@
+import time
+from selenium.webdriver.common.by import By
+def test_button_add_to_basket_exist_on_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    time.sleep(30)
+    assert browser.find_element(By.CSS_SELECTOR,'.btn.btn-lg.btn-primary.btn-add-to-basket')
